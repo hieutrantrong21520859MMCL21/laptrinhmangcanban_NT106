fba4d9f4eb5bda41ebec86fd6b6db29654ad3c97
DESKTOP-U7NVEFD\LEGION <hieutt03112003@gmail.com>
2023-04-18 22:32:33 +0700

    3rd commit

---
+++ Lab2/Bai6/Bai6.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Bai6
+{
+    partial class Bai6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeFolder = new System.Windows.Forms.TreeView();
+            this.grbBox = new System.Windows.Forms.GroupBox();
+            this.picResult = new System.Windows.Forms.PictureBox();
+            this.txtView = new System.Windows.Forms.TextBox();
+            this.grbBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // treeFolder
+            // 
+            this.treeFolder.Location = new System.Drawing.Point(12, 12);
+            this.treeFolder.Name = "treeFolder";
+            this.treeFolder.Size = new System.Drawing.Size(268, 426);
+            this.treeFolder.TabIndex = 0;
+            this.treeFolder.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeFolder_BeforeExpand);
+            this.treeFolder.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeFolder_NodeMouseClick);
+            // 
+            // grbBox
+            // 
+            this.grbBox.Controls.Add(this.txtView);
+            this.grbBox.Controls.Add(this.picResult);
+            this.grbBox.Location = new System.Drawing.Point(286, 13);
+            this.grbBox.Name = "grbBox";
+            this.grbBox.Size = new System.Drawing.Size(604, 425);
+            this.grbBox.TabIndex = 1;
+            this.grbBox.TabStop = false;
+            this.grbBox.Text = "File Content";
+            // 
+            // picResult
+            // 
+            this.picResult.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picResult.Location = new System.Drawing.Point(3, 18);
+            this.picResult.Name = "picResult";
+            this.picResult.Size = new System.Drawing.Size(598, 404);
+            this.picResult.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picResult.TabIndex = 0;
+            this.picResult.TabStop = false;
+            // 
+            // txtView
+            // 
+            this.txtView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtView.Location = new System.Drawing.Point(3, 18);
+            this.txtView.Multiline = true;
+            this.txtView.Name = "txtView";
+            this.txtView.Size = new System.Drawing.Size(598, 404);
+            this.txtView.TabIndex = 1;
+            // 
+            // Bai6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(902, 450);
+            this.Controls.Add(this.grbBox);
+            this.Controls.Add(this.treeFolder);
+            this.Name = "Bai6";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grbBox.ResumeLayout(false);
+            this.grbBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picResult)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeFolder;
+        private System.Windows.Forms.GroupBox grbBox;
+        private System.Windows.Forms.PictureBox picResult;
+        private System.Windows.Forms.TextBox txtView;
+    }
+}
+
---
+++ Lab2/Bai6/Bai6.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai6
+{
+    public partial class Bai6 : Form
+    {
+        public Bai6()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // lay danh sach cac o dia
+            string[] drives = Directory.GetLogicalDrives();
+
+            TreeNode root;
+
+            // duyet tung phan tu (ten o dia), them vao TreeView
+            foreach (string drv in drives)
+            {
+                root = new TreeNode(drv);
+
+                // them root vao TreeView
+                treeFolder.Nodes.Add(root);
+                root.Tag = drv;
+
+                // them node con vao root
+                root.Nodes.Add("");
+            }
+        }
+
+        private void treeFolder_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            TreeNode node = e.Node;
+            string path = (string)node.Tag;
+            FileAttributes attr = File.GetAttributes(path);
+            if (attr.HasFlag(FileAttributes.Directory) || Directory.GetLogicalDrives().Contains(path))
+            {
+                TreeViewCancelEventArgs new_e = new TreeViewCancelEventArgs(node, false, TreeViewAction.ByMouse);
+                treeFolder_BeforeExpand(sender, new_e);
+            }
+            else
+            {
+                if (path.ToLower().EndsWith(".jpg") || path.ToLower().EndsWith(".png") || path.ToLower().EndsWith(".jpeg"))
+                {
+                    picResult.Visible = true;
+                    picResult.SizeMode = PictureBoxSizeMode.StretchImage;
+                    picResult.Image = Image.FromFile(path);
+                    txtView.Visible = false;
+                }
+                else // doc file text
+                {
+                    txtView.Visible = true;
+                    FileStream fs = File.Open(path, FileMode.Open);
+                    StreamReader sr = new StreamReader(fs);
+                    txtView.Text = sr.ReadToEnd();
+                    fs.Close();
+                    picResult.Visible = false;
+                }
+            }
+        }
+
+        private void treeFolder_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            // xac dinh node da chon
+            TreeNode node = e.Node;
+
+            // xoa node rong
+            node.Nodes.Clear();
+
+            // lay danh sach cac thu muc con
+            string[] sub_folders = Directory.GetDirectories(node.FullPath);
+
+            // lay danh sach cac file
+            string[] files = Directory.GetFiles(node.FullPath);
+
+            try
+            {
+                // duyet qua danh sach cac thu muc con, kiem tra neu con cac thu muc con khac
+                foreach (string dir in sub_folders)
+                {
+                    DirectoryInfo di = new DirectoryInfo(dir);
+                    TreeNode child = node.Nodes.Add(di.Name);
+                    child.Tag = di.FullName;
+                    child.Nodes.Add("");
+                }
+
+                // duyet qua danh sach cac file trong thu muc
+                foreach (string file in files)
+                {
+                    FileInfo fi = new FileInfo(file);
+                    TreeNode child = node.Nodes.Add(fi.Name);
+                    child.Tag = fi.FullName;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
